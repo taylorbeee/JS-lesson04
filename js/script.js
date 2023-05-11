d81ca05d3543ada46553a53a7593be521547ce56
Taylor Behrens <126136711+taylorbeee@users.noreply.github.com>
2023-05-11 15:03:18 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var footer = document.querySelector("footer");
+footer.style.backgroundColor = "#d7ddf3";
+
+// console.log(footer);
+
+var tagline = document.querySelector(".tagline");
+tagline.innerText = "as ugly as his drawings...";
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
